1f18917db2d69712c0bb51273fe1fd2c32b51034
Matt <98755798+m-inwards@users.noreply.github.com>
2022-07-12 16:33:29 +0100

    Testing

--- js/index.js
+++ js/index.js
@@ -2,14 +2,12 @@
  * Hero type effect
  */
 const typed = document.getElementById('typed');
-if (typed) {
-  let typed_strings = typed.getAttribute('data-typed-items')
-  typed_strings = typed_strings.split(',')
-  new Typed('#typed', {
-    strings: typed_strings,
-    loop: true,
-    typeSpeed: 100,
-    backSpeed: 50,
-    backDelay: 2000
+let typed_strings = typed.getAttribute('data-typed-items');
+typed_strings = typed_strings.split(',');
+
+while (true) {
+  typed_strings.forEach((item, i) => {
+    console.log(item);
   });
+
 }
