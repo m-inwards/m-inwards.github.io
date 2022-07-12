8159ef21ed052a3b13ee3acd7f26db4ad8109b16
Matt <98755798+m-inwards@users.noreply.github.com>
2022-07-12 16:05:23 +0100

    Testing JS

--- js/index.js
+++ js/index.js
@@ -1,7 +1,7 @@
 /**
  * Hero type effect
  */
-const typed = select('.typed')
+const typed = document.getElementById('typed');
 if (typed) {
   let typed_strings = typed.getAttribute('data-typed-items')
   typed_strings = typed_strings.split(',')
