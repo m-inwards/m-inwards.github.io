b7f151b90adadcfba57f19f6f94f4e4a3457298f
Matt <98755798+m-inwards@users.noreply.github.com>
2022-07-12 16:47:46 +0100

    Testing

--- js/index.js
+++ js/index.js
@@ -7,7 +7,17 @@ typed_strings = typed_strings.split(',');
 
 while (true) {
   typed_strings.forEach((item, i) => {
-    typed.innerHTML = item;
+    var i = 0;
+    var txt = item;
+    var speed = 50;
+
+    function typeWriter() {
+      if (i < txt.length) {
+        typed.innerHTML += txt.charAt(i);
+        i++;
+        setTimeout(typeWriter, speed);
+      }
+    }
   });
 
 }
