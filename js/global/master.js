78949b4332788404748adfbd5827580613792f10
Matt <98755798+m-inwards@users.noreply.github.com>
2023-08-07 22:45:47 +0100

    Update

--- js/global/master.js
+++ js/global/master.js
@@ -1,7 +1,7 @@
-fetch('/html/header.html')
+fetch('/header.html')
     .then(x=> x.text())
     .then(y=> document.getElementById('header').innerHTML = y)
 
-fetch('/html/footer.html')
+fetch('/footer.html')
     .then(x=> x.text())
     .then(y=> document.getElementById('footer').innerHTML = y)
