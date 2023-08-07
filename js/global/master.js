122262ca34294d62ec6e545ddad52607205870fa
Matt <98755798+m-inwards@users.noreply.github.com>
2023-08-07 22:54:31 +0100

    Update

--- js/global/master.js
+++ js/global/master.js
@@ -5,12 +5,3 @@ fetch('/header.html')
 fetch('/footer.html')
     .then(x=> x.text())
     .then(y=> document.getElementById('footer').innerHTML = y)
-
-function setFavicons(fav_img){
-    let headTitle = document.querySelector('head');
-    let favicon = document.createElement('link');
-    favicon.setAttribute('rel','shortcut icon');
-    favicon.setAttribute('href',fav_img);
-    headTitle.appendChild(favicon);
-}
-setFavicons('/media/profile_pic.png');
